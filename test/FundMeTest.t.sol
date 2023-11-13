51c37cb66252ab67d8e7a7ca0954d29f43ad708c
Que5 <phumlanibrightdlamini@gmail.com>
2023-11-13 08:52:14 +0200

    tested the msg sender

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -8,12 +8,19 @@ import {FundMe} from "../src/FundMe.sol";
 
 
 contract FundMeTest is Test{
+
+    FundMe fundMe;
+
     function setUp() external {
-        FundMe fundMe = new FundMe();
+        fundMe = new FundMe();
 
     }
 
-    function testDemo() public {
+    function testMinimumDollarIsFive() public {
+        assertEq(fundMe.MINIMUM_USD(), 5e18);
 
+    }
+    function testOwnerIsMsgSender() public {
+        assertEq(fundMe.i_owner(), address(this));
     }
 }
